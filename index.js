ebf56b5e83ec84e36753b52e67c59748d8706f0e
ayanmirza99 <125931448+ayanmirza99@users.noreply.github.com>
2023-04-13 07:30:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function sumofEvenNumbers(numbers) {
   }
   return evenSum;
 }
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16];
+const numbers = [1,2,3,4,5,6,7,8,9]
 const evenSum = sumofEvenNumbers(numbers);
 console.log(evenSum);
 
