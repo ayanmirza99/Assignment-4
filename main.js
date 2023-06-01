8868f0d8a4f0f5c7c23818f2928f5297295f25d5
ayanmirza99 <125931448+ayanmirza99@users.noreply.github.com>
2023-06-01 19:40:22 +0500

    Update main.js

--- main.js
+++ main.js
@@ -73,3 +73,72 @@ let mystring = "Ayan Mirza"
 let myvowels = countVowels(mystring)
 console.log(mystring)
 console.log(`Number of vowels in "${mystring}" = ` + myvowels)
+
+
+
+// Write a function that takes an array of strings as input and returns the length of the longest string in the array.
+
+
+function getLongestStr(arr) {
+  let longestStr = {
+    string: "",
+    length: 0,
+  };
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > longestStr.length) {
+      longestStr.string = arr[i];
+      longestStr.length = arr[i].length;
+    }
+  }
+  return longestStr;
+}
+
+let array = ["Ayan" , "Ahmed" , "Mustafa" ,"Salman"];
+let longestString = getLongestStr(array);
+console.log(`Longest string inside the array "${array}" is "${longestString.string} and its length is ${longestString.length}"`);
+
+
+//  Write a function that takes an array of objects as input and returns an array of all the values for a specified key in each object.
+
+let students = [
+  {
+    name: "Ayan Mirza",
+    fatherName: "Muhammad Hafeez",
+    email: "ayan@gmail.com",
+    address: "Karachi, Pakistan",
+  },
+  {
+    name: "Bilal Ahmed",
+    fatherName: "Arshad",
+    email: "bilal@gmail.com",
+    address: "Karachi, Pakistan",
+  },
+  {
+    name: "Abdul Moiz",
+    fatherName: "Arshad",
+    email: "moiz@gmail.com",
+    address: "Karachi, Pakistan",
+  },
+];
+
+function getSpecificKeyValues(arr, key) {
+  const values = [];
+  for (let i = 0; i < arr.length; i++) {
+    values.push(arr[i][key]);
+  }
+  return values;
+}
+let specificKey = "email";
+console.log(getSpecificKeyValues(students,specificKey));
+
+
+
+// Write a function that takes an array of strings as input and returns an array of all the unique strings in the array.
+
+const getUniqueStrings = (arr) => {
+  return [new Set(arr)];
+}
+
+const Fruits = ["apple", "banana", "apple", "pear", "pear", "orange"];
+const uniqueFruitArr = getUniqueStrings(Fruits);
+console.log(uniqueFruitArr);
